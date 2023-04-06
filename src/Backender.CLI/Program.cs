eda64193c7fcb54abfb05a8a82c50f10d28ccf5a
Mahdi hoseinpoor <hoseinpoor2006@Gmail.com>
2023-04-07 00:37:58 +0330

    Make Cli better

--- src/Backender.CLI/Program.cs
+++ src/Backender.CLI/Program.cs
@@ -12,23 +12,36 @@ namespace Backender.ConsoleApp
 		
 			Engine app = new();
 
-			WriteMessage("-------------------- Welcome to Backender! --------------------", ConsoleColor.Blue);				
+			WriteMessage("===================== Welcome to Backender! =====================", ConsoleColor.Blue);
+			while (true)
+			{
 #if DEBUG
 			app.FileName = "G:\\shoping site.yaml";
 #else
-		
-			Console.WriteLine("--- Enter your Configuration File Location:");
-			app.FileName = Console.ReadLine()!.Trim();
+
+				WriteMessage("--- Enter your Configuration File Location:");
+				Console.Write(">>> ");
+				app.FileName = Console.ReadLine()!.Trim();
 #endif
-			WriteMessage($"--- {app.FileName} -> The Config File Processing has been start");
-			WriteMessage($"--- Backender Engine Start to Generate!");
-		 await app.Run();
-			Console.ForegroundColor = ConsoleColor.Green;
-			Console.Write($"--- Your Project Has Been Created in ");
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.Write(app.SavePath + "\n");
-			Console.ForegroundColor = ConsoleColor.White;
-			Console.ReadKey();
+				if (File.Exists(app.FileName))
+				{
+					WriteMessage($"--- {app.FileName} -> The Config File Processing has been start");
+					WriteMessage($"--- Backender Engine Start to Generate!");
+					await app.Run();
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.Write($"--- Your Project Has Been Created in ");
+					Console.ForegroundColor = ConsoleColor.Yellow;
+					Console.Write(app.SavePath + "\n");
+					Console.ForegroundColor = ConsoleColor.White;
+					Console.ReadKey();
+					Environment.Exit(0);
+				}
+				else
+				{
+					WriteMessage($"---  I can not found Config file in '{app.FileName}'", ConsoleColor.Red);
+				}
+				
+			}
 
 			//app.Run("G:\\Shopping Site.json", "G:\\NewShoppingSite");
 
