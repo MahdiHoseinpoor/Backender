f3438d4fb5f001f1b8500fef2dd19433dbe367c0
Shupsta <Shupsta2010@gmail.com>
2023-04-25 18:17:58 -0700

    Put Block.cs in the PalinoSerializer as its the only class that uses it.

--- src/Backender.Translator/PalinoSerializer.cs
+++ src/Backender.Translator/PalinoSerializer.cs
@@ -107,4 +107,12 @@
 			return Blocks;
 		}
 	}
+
+	public class Block
+	{
+		public string Type { get; set; }
+		public string Value { get; set; }
+		public List<string> Options { get; set; } = new List<string>();
+		public List<Block> InnerBlocks { get; set; } = new List<Block>();
+	}
 }
